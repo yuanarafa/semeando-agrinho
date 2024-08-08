824e294d96d919d4f56cb4c83eec7d244f7ad765
profbrunabf <bruna.bueno.ferreira@escola.pr.gov.br>
2024-08-08 15:44:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetID = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetID);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+
+    // Adiciona funcionalidade ao formulário de contato
+    const contatoForm = document.getElementById('contato-form');
+    if (contatoForm) {
+        contatoForm.addEventListener('submit', function(e) {
+            e.preventDefault();
+            const nome = document.getElementById('nome').value;
+            const email = document.getElementById('email').value;
+            const mensagem = document.getElementById('mensagem').value;
+
+            console.log('Nome:', nome);
+            console.log('Email:', email);
+            console.log('Mensagem:', mensagem);
+
+            alert('Mensagem enviada com sucesso!');
+        });
+    }
+});
+
+document.querySelector('.entrada').addEventListener('animationend', function() {
+    console.log('A animação terminou!');
+    
+    this.classList.add('animated');
+});
